c57e20ef8d1d7a6ca44a474a223ea9cbe933ffdc
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-02-17 20:59:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-fol1,fol2=st.columns(2)
+fol1,fol2=st.beta_columns(2)
 with fol1:
     st.title("                            BristolApp")
 
@@ -7,50 +7,58 @@ with fol2:
     st.image("WAPP.png",None,350)
 
 st.subheader("Identifica el tipo de evacuaciones que tienes y verémos si necesitas ajustar tu dieta")
-col1,col2,col3,col4,col5,col6,col7=st.columns(7)
+col1,col2,col3,col4,col5,col6,col7=st.beta_columns(7)
 
 with col1:
     st.image('B1.png')
     st.markdown("Pedazos duros separados. Como nueces y con excreción difícil")
-    bri1=st.button("Este")
-    st.camera_input("Take a photo")
+    bri1=st.checkbox("B1")
 with col2:
     st.image("B2.png")
     st.markdown("Con forma de salchicha, pero grumosa (compuesta de fragmentos")
-    bri2=st.checkbox('este')
+    bri2=st.checkbox('B2')
 with col3:
     st.image("B3.png")
     st.markdown("Con forma de salchica, pero con grietas en la superficie")
-    bri3=st.selectbox('este',['No','Esta'])
+    bri3=st.checkbox('B3')
 with col4:
     st.image("B4.png")
     st.markdown('Con forma de salchica pero lisa y suave')
-    st.radio('Elige','Esta')
+    bri4=st.checkbox('B4')
 with col5:
     st.image('B5.png')
     st.markdown("Trozos de pastosos con bordes bien definidos")
+    bri5=st.checkbox('B5')
 with col6:
     st.image('B6.png')
     st.markdown("Pedazos blandos y esponjosos con bordes irregulares")
+    bri6=st.checkbox('B6')
 with col7:
     st.image('B7.png')
     st.markdown('Acuosa, sin pedazos sólidos, totalmente líquida')
+    bri7=st.checkbox('B7')
+if bri1==True:
+    st.info("Característico del estreñimiento, debes aumentar el consumo de fibra, o iniciar el consumo de fibra en suplementos como el Psyllium plantago, 1 cucharada con 1 litro de agua diariamente")
+if bri2==True:
+    st.info('Característico del estreñimiento, debes aumentar el consumo de fibra, o iniciar el consumo de fibra en suplementos como el Psyllium plantago, 1 cucharada con 1 litro de agua diariamente')
+        # if bri2==True and bri1==False:
+        #     st.info("Característico del estreñimiento, debes aumentar el consumo de fibra, o iniciar el consumo de fibra en suplementos como el Psyllium plantago, 1 cucharada con 1 litro de agua diariamente")
+        # else:
+        #     st.warning("Elige una opción")
+        # if bri3==True:
+        #     st.success("Excelente, tus heces son normales no debes realizar cambios en tu dieta respecto a la forma de tus heces")
+        #     st.balloons()
 
 
-#     st.info("Característico del estreñimiento, debes aumentar el consumo de fibra, o iniciar el consumo de fibra en suplementos como el Psyllium plantago, 1 cucharada con 1 litro de agua diariamente")
-# elif sel=='2':
-#     with col1:
-#         st.image('B2.png')
-#     with col2:
-#         st.subheader("Con forma de salchica, pero grumosa (compuesta de fragmentos)")
-#     st.info("Característico del estreñimiento, debes aumentar el consumo de fibra, o iniciar el consumo de fibra en suplementos como el Psyllium plantago, 1 cucharada con 1 litro de agua diariamente")
+
+
+#
 
 # elif sel=='3':
 #     with col1:
 #         st.image('B3.png')
 #     with col2:
 #         st.subheader("Con forma de Salchica, pero con grietas en la superficie")
-#     st.success("Excelente, tus heces son normales no debes realizar cambios en tu dieta respecto a la forma de tus heces")
 #     st.balloons()
 # elif sel=='4':
 #     with col1:
