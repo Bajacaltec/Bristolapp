4db1861c2e838658bdf8170e10cca3695c92c0f1
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-02-17 21:01:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ if bri1==True:
 if bri2==True:
     st.info('Característico del estreñimiento, debes aumentar el consumo de fibra, o iniciar el consumo de fibra en suplementos como el Psyllium plantago, 1 cucharada con 1 litro de agua diariamente')
 if bri3==True:
-    st.info('Excelente, tus heces son normales no debes realizar cambios en tu dieta respecto a la forma de tus heces")')
+    st.success('Excelente, tus heces son normales no debes realizar cambios en tu dieta respecto a la forma de tus heces")')
+    st.balloons()
         # if bri2==True and bri1==False:
         #     st.info("Característico del estreñimiento, debes aumentar el consumo de fibra, o iniciar el consumo de fibra en suplementos como el Psyllium plantago, 1 cucharada con 1 litro de agua diariamente")
         # else:
