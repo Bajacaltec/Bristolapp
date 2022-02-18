74bb494a0e20d5acd365a82aa85f50bc42bb04bc
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-02-17 22:04:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,25 +11,31 @@ st.subheader("Identifica el tipo de evacuaciones que tienes y verémos si necesi
 col1,col2,col3,col4,col5,col6,col7=st.columns(7)
 
 with col1:
-    st.subheader('Bristol1 (B1)')
+    st.subheader('B1')
     st.image('B1.png')
     st.markdown("Pedazos duros separados. Como nueces y con excreción difícil")
 with col2:
+    st.subheader('B2')
     st.image("B2.png")
     st.markdown("Con forma de salchicha, pero grumosa (compuesta de fragmentos")
 with col3:
+    st.subheader("B3")
     st.image("B3.png")
     st.markdown("Con forma de salchica, pero con grietas en la superficie")
 with col4:
+    st.subheader('B4')
     st.image("B4.png")
     st.markdown('Con forma de salchica pero lisa y suave')
 with col5:
+    st.subheader("B5")
     st.image('B5.png')
     st.markdown("Trozos de pastosos con bordes bien definidos")
 with col6:
+    st.subheader("B6")
     st.image('B6.png')
     st.markdown("Pedazos blandos y esponjosos con bordes irregulares")
 with col7:
+    st.subheader("B7")
     st.image('B7.png')
     st.markdown('Acuosa, sin pedazos sólidos, totalmente líquida')
 selecccion=st.select_slider('Elige',['B1','B2','B3','B4','B5','B6','B7'])
