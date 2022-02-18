d971b4f42d54f2a090798e66f07e74cc75a071c0
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-02-17 21:42:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ with fol1:
 with fol2:
     st.image("WAPP.png",None,350)
 
+col1,col2=st.columns(2):
+with col1:
+    st.image('B1.png')
+    st.image('B2.png')
+with col2:
+    rad=st.radio("Elige el tipo de evacuaciones",['Bristol1','Bristol2'])
+
 st.subheader("Identifica el tipo de evacuaciones que tienes y verémos si necesitas ajustar tu dieta")
 col1,col2,col3,col4,col5,col6,col7=st.columns(7)
 
@@ -52,8 +59,8 @@ if bri3==True:
         st.success('Excelente, tus heces son normales no debes realizar cambios en tu dieta respecto a la forma de tus heces')
     st.balloons()
 if bri4==True:
-    st.subheader('B4')
     with col4:
+        st.subheader('B4')
         st.success('Excelente, tus heces son normales no debes realizar cambios en tu dieta respecto a la forma de tus heces')
     st.balloons()
 if bri5==True:
